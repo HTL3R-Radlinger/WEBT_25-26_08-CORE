ad6f0822f73c296efb82e61f61ff16ffd1ed2775
AlexBa16 <alexbachinger56+GitHub@gmail.com>
2026-06-08 15:19:25 +0200

    remove unused files

--- packages/fortune/Classes/Domain/Repository/MessageRepository.php
+++ packages/fortune/Classes/Domain/Repository/MessageRepository.php
@@ -6,30 +6,16 @@ namespace Htl3r\Fortune\Domain\Repository;
 
 use Htl3r\Fortune\Domain\Model\Message;
 use TYPO3\CMS\Extbase\Persistence\Repository;
-use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
 
 class MessageRepository extends Repository
 {
-//    public function initializeObject(): void
-//    {
-//        /** @var Typo3QuerySettings $querySettings */
-//        $querySettings = $this->createQuery()->getQuerySettings();
-//        $querySettings->setRespectStoragePage(true);
-//        $querySettings->setStoragePageIds([45]);
-//
-//        $this->setDefaultQuerySettings($querySettings);
-//    }
-
     public function findMessageOfTheDay(): ?Message
     {
-//        $query = $this->createQuery();
-//        $all = $query->execute()->toArray();
         $all = $this->findAll()->toArray();
 
         if (empty($all)) {
             return null;
         }
-
         $daySeed = (int)date('z') + (int)date('Y') * 366;
         $index = $daySeed % count($all);
 
